e7a5697c17f112e31f2491cead289cc17ae854f4
Nélia Simas <97136166+NSimas@users.noreply.github.com>
2024-05-19 20:04:27 +0000

    Atividade simples da DIO

---
+++ Exercícios/Longest Consecutive Sequence/LeetCode 128 - Longest Consecutive Sequence/MaiorOuMenor/maiorOuMenor.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      int saldoTotal = int.Parse(Console.ReadLine());
+
+      int valorSaque = int.Parse(Console.ReadLine());
+      
+      if (valorSaque <= saldoTotal)
+      {
+        saldoTotal = saldoTotal - valorSaque;
+        Console.WriteLine("Saque realizado com sucesso. Novo saldo: " + saldoTotal);
+      }
+      else
+      {
+        Console.WriteLine("Saldo insuficiente. Saque nao realizado!");
+      }
+    }
+}
