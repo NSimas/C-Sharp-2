60a4b413c50433b7eb1503e5a764dfbb5f26b971
Nélia Simas <neliasimas@hotmail.com>
2024-07-09 11:37:52 -0300

    Add

--- Exercicio0002/Longest Consecutive Sequence/LeetCode 128 - Longest Consecutive Sequence/obj/Debug/net8.0/LeetCode 128 - Longest Consecutive Sequence.AssemblyInfo.cs
+++ Exercicio0002/Longest Consecutive Sequence/LeetCode 128 - Longest Consecutive Sequence/obj/Debug/net8.0/LeetCode 128 - Longest Consecutive Sequence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode 128 - Longest Consecutive Sequence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04feeefced57f44b10171730e200f2040b51a86c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f2b92a8ee0ed0bfa78221270130b05e81076996")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode 128 - Longest Consecutive Sequence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode 128 - Longest Consecutive Sequence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
